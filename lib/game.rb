ce2ca08e6ab521dc3241e4623d21ba403d47e7a2
urilixo <falecomelvio@hotmail.com>
2021-12-09 16:12:27 -0300

    Added word randomizer, display and guessing

--- lib/game.rb
+++ lib/game.rb
@@ -7,8 +7,31 @@
 require 'pry-byebug'
 class Game
   def initialize(dictionary = 'clean_dictionary.txt', lines_in_dictionary = 52_452)
+    @mistakes = 0
+    @secret_word = pick_random
+    @display = display_word(secret_word)
+    @finished = false
+    until finished?
+      guess = Guess.new
+      @mistakes += 1 if compare_guess(guess, @secret_word) == nil
+      finished = true if @display.join == @secret_word
+    end
     random_word = pick_random(dictionary, lines_in_dictionary)
     puts random_word
+  end
+
+  def already_guessed
+    'You have already guessed this character'
+  end
+
+  def compare_guess(guess, secret)
+    return already_guessed if @display.include?(guess)
+
+    array_of_indexes = []
+    secret.split('').each_with_index { |char, index| array_of_indexes << index if char == guess}
+    return nil if array_of_indexes.empty?
+
+    edit_display(guess, array_of_indexes)
   end
 
   def pick_random(dictionary, lines_in_dictionary)
@@ -20,6 +43,39 @@ class Game
       end
     end
     random_word
+  end
+
+  def display_word(secret)
+    display = []
+    secret.length.times do
+      display << ' _ '
+    end
+  end
+
+  def edit_display(character, array_of_indexes)
+    array_of_indexes.each { |index| @display[index] = character }
+  end
+
+  def game_over
+    puts 'You lost'
+  end
+end
+
+class Guess
+  def initialize
+    @guess = player_guess
+  end
+
+  def player_guess
+    guess = gets.chomp.downcase
+    return invalid_guess unless ('a'..'z').include?(guess)
+
+    guess
+  end
+
+  def invalid_guess
+    puts 'Guesses must be a single character from A to Z'
+    player_guess
   end
 end
 
